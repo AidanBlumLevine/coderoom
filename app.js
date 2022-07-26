9fd6adef98919c32ef307cbe0e715f80273a7db5
aidanzev <aidanblumlevine@gmail.com>
2022-07-25 23:10:19 -0400

    fix port 2.0

--- app.js
+++ app.js
@@ -1,56 +1,58 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var express = require('express')
+var app = express()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
-var _rooms = require('./src/room');
-var Rooms = new _rooms(io);
+var _rooms = require('./src/room')
+var Rooms = new _rooms(io)
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+    res.sendFile(__dirname + '/public/index.html')
+})
 
 io.on('connection', (socket) => {
     socket.on('name', (name) => {
-        socket.username = name;
-        socket.emit('name', name);
-    });
+        socket.username = name
+        socket.emit('name', name)
+    })
     socket.on('create', () => {
-        Rooms.add(socket);
-    });
+        Rooms.add(socket)
+    })
 
     socket.on('room', (id) => {
         if (socket.rooms[id] == undefined && Rooms.find(id) != undefined) {
-            Rooms.join(socket, id);
+            Rooms.join(socket, id)
         } else {
-            socket.emit('bad-room', id);
+            socket.emit('bad-room', id)
         }
-    });
+    })
 
     socket.on('update', (code) => {
-        Rooms.update(socket, code);
-    });
+        Rooms.update(socket, code)
+    })
 
     socket.on('disconnect', () => {
-        Rooms.disconnect(socket);
-    });
+        Rooms.disconnect(socket)
+    })
 
     socket.on('can_reconnect', (info) => {
-        if(Rooms.can_reconnect(info)){
-            socket.emit('can_reconnect');
+        if (Rooms.can_reconnect(info)) {
+            socket.emit('can_reconnect')
         }
-    });
+    })
 
     socket.on('do_reconnect', (info) => {
-        Rooms.reconnect(socket, info);
-    });
-});
+        Rooms.reconnect(socket, info)
+    })
+})
 
 setInterval(function () {
-    Rooms.update_clients();
-}, 1000);
+    Rooms.update_clients()
+}, 1000)
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log('listening on '. process.env.PORT || 3000);
-});
+let port = process.env.PORT || 3000
+
+http.listen(port, () => {
+    console.log('listening on '+port)
+})
